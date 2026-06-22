aafcdb1b24d3c07b6e07e527fee1495f36da5ef5
devart-coder <devart.mail@mail.ru>
2026-06-22 19:43:31 +0300

    working with table_view

--- src/application.rs
+++ src/application.rs
@@ -1,35 +1,42 @@
-use std::fmt::format;
+use std::{
+    collections::HashSet,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
-use calamine::{Reader, SheetVisible, Xls, open_workbook, open_workbook_auto};
-use egui::{Response, RichText, Ui};
+use calamine::{Reader, SheetVisible};
+use egui::{RichText, Ui};
 use egui_extras::{Column, TableBuilder};
 
 use crate::{
-    agregator::{Agregator, Status},
+    agregator::{self, Agregator, Status},
     document::Document,
 };
 
 #[derive(Default)]
 pub struct Application {
+    selected: String,
+    file_opened: bool,
     workbook: Document,
     agregators: Vec<Agregator>,
-    search_by: RichText,
+    search_by: HashSet<String>,
 }
 impl Application {
-    fn create_top(&mut self, ui: &mut Ui) {
+    fn create_head(&mut self, ui: &mut Ui) {
         ui.heading("Настройки");
         ui.horizontal(|ui| {
             ui.group(|ui| {
                 ui.set_min_width(ui.available_width());
                 ui.heading("Искать");
                 let _ = egui::ComboBox::from_id_salt("selected")
-                    .selected_text(format!("{}", self.search_by.text()))
+                    .selected_text(&self.selected)
                     .show_ui(ui, |ui| {
-                        ui.selectable_value(
-                            &mut self.search_by,
-                            RichText::from("First").heading(),
-                            RichText::from("First").heading(),
-                        );
+                        for item in &self.search_by {
+                            ui.selectable_value(
+                                &mut self.selected,
+                                item.to_string(),
+                                item.to_string(),
+                            );
+                        }
                     });
                 let _ = ui.heading("Агрегатор");
                 if ui.button("Добавить").clicked() {
@@ -40,29 +47,52 @@ impl Application {
         });
     }
     fn create_table(&mut self, ui: &mut Ui) {
-        TableBuilder::new(ui)
-            .column(Column::auto().resizable(true))
-            .column(Column::remainder())
-            .header(20.0, |mut header| {
-                header.col(|ui| {
-                    ui.heading("First column");
-                });
-                header.col(|ui| {
-                    ui.heading("Second column");
-                });
-            })
-            .body(|mut body| {
-                body.row(30.0, |mut row| {
-                    row.col(|ui| {
-                        ui.label("Hello");
+        egui::ScrollArea::both().show(ui, |ui| {
+            TableBuilder::new(ui)
+                .column(Column::auto().resizable(true))
+                .striped(true)
+                .column(Column::remainder())
+                .header(20.0, |mut header| {
+                    header.col(|ui| {
+                        ui.strong(self.selected.to_string());
                     });
-                    row.col(|ui| {
-                        ui.button("world!");
+                    header.col(|ui| {
+                        for agregator in &self.agregators {
+                            for text in agregator.selected() {
+                                ui.strong(text);
+                            }
+                        }
                     });
-                });
-            });
-    }
+                })
+                .body(|mut body| {
+                    body.row(50.0, |mut row| {
+                        let draw_cell = |ui: &mut egui::Ui, text: String| {
+                            // 1. Рисуем рамку вокруг ячейки (эффект Excel-сетки)
+                            let stroke = egui::Stroke::new(
+                                1.0,
+                                ui.visuals().widgets.noninteractive.bg_stroke.color,
+                            );
+                            ui.painter().rect_stroke(
+                                ui.max_rect(),
+                                0.0,
+                                stroke,
+                                egui::StrokeKind::Inside,
+                            );
 
+                            // 2. Добавляем внутренний отступ, чтобы текст не прилипал к линиям
+                            ui.add_space(4.0);
+                            ui.label(text);
+                        };
+                        row.col(|ui| {
+                            draw_cell(ui, self.selected.clone());
+                        });
+                        row.col(|ui| {
+                            draw_cell(ui, self.selected.clone());
+                        });
+                    });
+                });
+        });
+    }
     fn create_menu(&mut self, ui: &mut Ui) {
         ui.menu_button("Файл", |ui| {
             if ui.button("📂Открыть").clicked() {
@@ -70,36 +100,15 @@ impl Application {
                     .add_filter("Exel files", &["xlsx", "xlsm", "xls", "xlsb"]);
                 if let Some(file_path) = file_dialog.pick_file() {
                     if let Some(path_str) = file_path.to_str() {
-                        #[cfg(debug_assertions)]
-                        {
-                            println!("Path_str: {}", path_str);
-                        }
                         self.workbook = Document::open(path_str);
-                        // let excel = open_workbook_auto(file_path).unwrap();
-                        // #[cfg(debug_assertions)]
-                        // {
-                        // println!("Document was opened.",);
-                        // println!("Sheets range: {:?}", excel.sheet_names());
-                        // }
-                        #[cfg(debug_assertions)]
-                        {
-                            println!(
-                                "Visible sheets range: {:?}",
-                                self.workbook.sheets(SheetVisible::Visible)
-                            );
-                        }
-                        self.workbook.search_pos("№ п/п");
+                        self.search_by = self.workbook.search_pos("№ п/п").clone();
+                        self.file_opened = true;
                     }
                 }
-                // open_workbook("")
-            };
+            }
         });
     }
-}
-impl eframe::App for Application {
-    fn ui(&mut self, ui: &mut Ui, _frame: &mut eframe::Frame) {
-        self.create_menu(ui);
-        self.create_top(ui);
+    fn create_agregators_column(&mut self, ui: &mut Ui) {
         ui.horizontal(|ui| {
             ui.vertical(|ui| {
                 if self.agregators.is_empty() {
@@ -113,6 +122,9 @@ impl eframe::App for Application {
                             Status::None => {
                                 ui.push_id(index, |ui| {
                                     ui.label(format!("Агрегатор {}", index));
+                                    let mut clone = self.search_by.clone();
+                                    clone.remove(&self.selected);
+                                    item.multiselect(&clone);
                                     item.draw(ui);
                                 });
                             }
@@ -125,5 +137,14 @@ impl eframe::App for Application {
             });
             self.create_table(ui);
         });
+    }
+}
+impl eframe::App for Application {
+    fn ui(&mut self, ui: &mut Ui, _frame: &mut eframe::Frame) {
+        self.create_menu(ui);
+        if self.file_opened == true {
+            self.create_head(ui);
+            self.create_agregators_column(ui);
+        }
     }
 }
